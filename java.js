9ddf1a513e22f5ac48b77385e171f9f2204377a1
AA07 <alexanderadame.david@gmail.com>
2022-09-10 19:33:59 -0500

    commit

---
+++ java.js
@@ -0,0 +1,37 @@
+
+//Alert message if there is no input into text box, to put info in text box.
+
+document.querySelector('#push').onclick = function(){
+  if(document.querySelector('#newtask input').value.length == 0){
+    alert("Please enter Task!")
+  }
+
+//Buttons within tasks
+
+  else{
+    document.querySelector('#tasks').innerHTML += `
+      <div class="task">
+        <span id="taskname">
+          ${document.querySelector('#newtask input').value}
+        </span>
+        <button class="delete" style="font-size:10px">X<i class="fa fa-remove">
+        </i></button>
+
+        <button class="finished" style="font-size:12px">Selector<i class="fa fa-toggle-off">
+        </i></button>
+      </div>`;
+
+    var current_tasks = document.querySelectorAll(".delete");
+      for(var i=0; i<current_tasks.length; i++){
+        current_tasks[i].onclick = function(){
+        this.parentElement.remove();
+        }
+      }
+    var strike_tasks = document.querySelectorAll(".finished");
+      for(var i=0; i<strike_tasks.length; i++){
+        strike_tasks[i].onclick = function(){
+        this.parentElement.style.setProperty("text-decoration", "line-through");
+        }
+      }
+  }
+}
