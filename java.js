7407122ad4dad90641812143079be162a8daeb6c
AA07 <alexanderadame.david@gmail.com>
2022-09-19 21:48:08 -0500

    Code Review for Todo app

--- java.js
+++ java.js
@@ -14,24 +14,25 @@ document.querySelector('#push').onclick = function(){
         <span id="taskname">
           ${document.querySelector('#newtask input').value}
         </span>
-        <button class="delete" style="font-size:10px">X<i class="fa fa-remove">
-        </i></button>
+      <button class="delete" style="font-size:10px">X<i class="fa fa-remove">
+      </i></button>
 
-        <button class="finished" style="font-size:12px">Selector<i class="fa fa-toggle-off">
-        </i></button>
+      <input type="checkbox" class ="finished" style="font-size:12px"><i class="fa fa-toggle-off">
+      </i></checkbox>
       </div>`;
+      
+  var current_tasks = document.querySelectorAll(".delete");
+    for(var i=0; i<current_tasks.length; i++){
+      current_tasks[i].onclick = function(){
+      this.parentElement.remove();
+      }
+    }
 
-    var current_tasks = document.querySelectorAll(".delete");
-      for(var i=0; i<current_tasks.length; i++){
-        current_tasks[i].onclick = function(){
-        this.parentElement.remove();
-        }
+  var strike_tasks = document.querySelectorAll(".finished");
+    for(var i=0; i<strike_tasks.length; i++){
+      strike_tasks[i].onclick = function(){
+      this.parentElement.style.setProperty("text-decoration", "line-through");
       }
-    var strike_tasks = document.querySelectorAll(".finished");
-      for(var i=0; i<strike_tasks.length; i++){
-        strike_tasks[i].onclick = function(){
-        this.parentElement.style.setProperty("text-decoration", "line-through");
-        }
-      }
+    }
   }
 }
